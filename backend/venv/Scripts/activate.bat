0fa2755aa9ff9da2a97cfc4198b694bbd3acf613
DELL <tayyabasohail2002@gmail.com>
2026-05-30 18:13:04 +0500

    initial commit

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\DELL\OneDrive\Documents\ai_tutor_app\backend\venv
+set VIRTUAL_ENV=C:\app\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
